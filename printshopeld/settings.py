4d83859ae84fbc0aa02c01b078156c9cd1e529b7
AbelShikanda <66814592+AbelShikanda@users.noreply.github.com>
2024-05-14 01:27:44 +0300

    Update settings.py

--- printshopeld/settings.py
+++ printshopeld/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'printshopeld',  
         'USER': 'root',  
-        'PASSWORD': 'root',  
+        'PASSWORD': '',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
